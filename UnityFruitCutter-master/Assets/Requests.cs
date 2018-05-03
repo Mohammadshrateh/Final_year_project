5799a4656ca556eef497fa71658aae9836811842
muhammad shuriteh <m.n.shrateh@gmail.com>
2018-05-03 20:34:11 +0300

    merge with marwan

---
+++ UnityFruitCutter-master/Assets/Requests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class Requests : MonoBehaviour {
+
+	void Start() {
+		StartCoroutine(GetText());
+	}
+
+	IEnumerator GetText() {
+		UnityWebRequest www = UnityWebRequest.Get("https://fun-treatment.firebaseio.com/data/0.json");
+		yield return www.SendWebRequest();
+
+		if(www.isNetworkError || www.isHttpError) {
+			Debug.Log(www.error);
+		}
+		else {
+			// Show results as text
+			Debug.Log("response");
+
+			Debug.Log(www.downloadHandler.text);
+
+			JObject 
+
+
+
+
+		}
+	}
+}
---
+++ UnityFruitCutter-master/Assets/UIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIController : MonoBehaviour {
+
+
+
+	public void playButtonClickEvent(){
+		Debug.Log ("click");
+
+
+		SceneManager.LoadScene("Second Scene");
+	}
+
+	public void FruitCutterButtonClickEvent(){
+		Debug.Log ("click");
+
+
+		SceneManager.LoadScene("Scene1");
+	}
+
+}
